9966f4fc04ed0975fa1b6e53cd32f725b2b49b14
pactonal <mitchell.crane105@gmail.com>
2018-09-20 15:29:13 -0400

    Changes to lab02

--- lab02/speedConversion.c
+++ lab02/speedConversion.c
@@ -3,18 +3,31 @@
  * to the equivalent pace (min/mile)
  */ 
 
+
 #include <stdio.h>
+
 
 // Main function
 int main(void) {
+
+
     // Initialize variables
 	float speed,pace;
+
+
     // Accept input
 	printf("Enter your speed:");
+
 	scanf("%f", &speed);
+
+
     // Calculate pace
-	pace = speed/60.0;
+	pace = 60.0f / speed;
+
+
     // Print pace to screen	
 	printf("The pace is: %f\n", pace); 
+
+
 	return 0;
 }
