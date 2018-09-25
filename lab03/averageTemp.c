de012de950d27dbbe9d30484e53ddbf0121cedff
pactonal <mitchell.crane105@gmail.com>
2018-09-25 19:50:27 -0400

    Finished lab03?

--- lab03/averageTemp.c
+++ lab03/averageTemp.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-float convert(float fahr) {
-    return ((fahr - 32) * (5 / 9));
-}
+float convert(float);
 
-float average(float a,float b,float c,float d,float e) {
-    return (a + b + c + d + e) / 5;
-}
+float average(float,float,float,float,float); 
 
 int main(void) {
+    
     float temp1,temp2,temp3,temp4,temp5;
+    printf("Enter five Fahrenheit temperatures separated by slashes: ");
     scanf("%f/%f/%f/%f/%f",&temp1,&temp2,&temp3,&temp4,&temp5);
 
     float ctemp1 = convert(temp1);
@@ -19,7 +17,19 @@ int main(void) {
     float ctemp4 = convert(temp4);
     float ctemp5 = convert(temp5);
     
-    printf("%+.3f",average(temp1,temp2,temp3,temp4,temp5)); 
-    printf("%+.3f",average(ctemp1,ctemp2,ctemp3,ctemp4,ctemp5));
+    printf("The average temperature in F:");
+    printf("%+9.3f\n",average(temp1,temp2,temp3,temp4,temp5)); 
+    printf("The average temperature in C:");
+    printf("%+9.3f\n",average(ctemp1,ctemp2,ctemp3,ctemp4,ctemp5));
 
+}
+
+float convert(float fahr) {
+    float cels = ((fahr - 32) * (5.0f / 9.0f));
+    return cels;
+}
+
+float average(float a,float b,float c,float d,float e) {
+    float avg = (a + b + c + d + e) / 5;
+    return avg;
 }
